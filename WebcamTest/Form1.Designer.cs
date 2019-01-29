b4e7ac227ddd9fcab5b44b820b87daaa5c33ddee
Thai Le <tql9803@rit.edu>
2019-01-28 19:00:21 -0500

    Inital commit

---
+++ WebcamTest/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WebcamTest
+{
+    partial class WebcamTestApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(555, 117);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(682, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StartButtonClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(555, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Camera";
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(65, 50);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(373, 310);
+            this.videoSourcePlayer1.TabIndex = 4;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(682, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.StopButtonClick);
+            // 
+            // WebcamTestApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "WebcamTestApp";
+            this.Text = "WebcamTestApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WebcamTest/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace WebcamTest
+{
+    public partial class WebcamTestApp : Form
+    {
+        FilterInfoCollection videoDevices;
+
+        public WebcamTestApp()
+        {
+            InitializeComponent();
+            UpdateVideoDevices();
+        }
+
+        private void UpdateVideoDevices()
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            for (int i = 1, n = videoDevices.Count; i <= n; i++)
+            {
+                string cameraName = i + " : " + videoDevices[i - 1].Name;
+
+                comboBox1.Items.Add(cameraName);                
+            }
+        }
+
+        private void StopButtonClick(object sender, EventArgs e)
+        {
+            StopCamera();
+        }
+
+        private void StartButtonClick(object sender, EventArgs e)
+        {
+            StartCameras();
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+        }
+
+        private void StartCameras()
+        {
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[comboBox1.SelectedIndex].MonikerString);
+
+            videoSourcePlayer1.VideoSource = videoSource;
+            videoSourcePlayer1.Start();
+        }
+
+        private void StopCamera()
+        {
+            videoSourcePlayer1.SignalToStop();
+            videoSourcePlayer1.WaitForStop();
+        }
+
+        
+    }
+}
